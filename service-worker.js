c02dcbf1bf7965c079ee249027aab1085f42d2b3
probingia <164015237+probingia@users.noreply.github.com>
2025-08-12 21:26:23 +0200

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,30 @@
+const CACHE_NAME = 'dynamic-chunking-course-v1';
+const FILES_TO_CACHE = [
+  './Curso sobre paper Dynamic chunking.html',
+  './manifest.json',
+  './images/icon-192.png',
+  './images/icon-512.png'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        console.log('[ServiceWorker] Pre-caching offline page');
+        return cache.addAll(FILES_TO_CACHE);
+      })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+  );
+});
